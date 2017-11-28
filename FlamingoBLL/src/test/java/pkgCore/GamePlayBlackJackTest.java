5357211803a93b9e81a2bbdcdffebe532a22994b
HUNG <HUNG@HUNG-HP>
2017-11-27 23:43:42 -0500

    added test TODOs

--- FlamingoBLL/src/test/java/pkgCore/GamePlayBlackJackTest.java
+++ FlamingoBLL/src/test/java/pkgCore/GamePlayBlackJackTest.java
@@ -8,22 +8,22 @@ public class GamePlayBlackJackTest {
 
 	@Test
 	public void TestPlayerWinning() {
-		fail("Not yet implemented");
+		fail("Not yet implemented"); //TODO do these tests
 	}
 
 	@Test
 	public void TestPlayerLosing() {
-		
+		//TODO do these tests
 	}
 	
 	@Test
-	public void TestPlayerPush() {
-		
+	public void TestPlayerTie() {
+		//TODO do these tests
 	}
 	
 	@Test
 	public void TestTwoPlayersWinning()
 	{
-		
+		//TODO do these tests
 	}
 }
